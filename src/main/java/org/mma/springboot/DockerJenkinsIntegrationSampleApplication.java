924dcd115917c3928338651582c6ce06ad447f8e
Tariq Ahsan <tariqahsan@yahoo.com>
2022-05-08 07:13:23 -0400

    first commit

---
+++ src/main/java/org/mma/springboot/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package org.mma.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
