69000bc85e7db5405f16a1d092c27a6178892200
Tariq Ahsan <tariqahsan@yahoo.com>
2022-05-08 07:50:50 -0400

    second commit

--- src/main/java/org/mma/springboot/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/org/mma/springboot/DockerJenkinsIntegrationSampleApplication.java
@@ -2,10 +2,17 @@ package org.mma.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerJenkinsIntegrationSampleApplication {
 
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Testing out Spring Boot Docker with Jenkins Integration";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
 	}
